477df1a040d6f2be81e6d10a39f9dd7edafc4e0c
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-12-01 18:11:42 -0800

    complete chefs#index

---
+++ spec/features/chefs/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'the chefs index page' do
+  before :each do 
+    @stinky_petes = Restaurant.create!(name: "Stinky Pete's", city: "Redlands", star_rating: 1, osha_safety_certified: false)
+    @nice_smellin_sals = Restaurant.create!(name: "Nice Smellin' Sal's", city: "Redlands", star_rating: 5, osha_safety_certified: true)
+
+    @stephanie = @nice_smellin_sals.chefs.create!(name: "Stephanie Cobalt", age: 27, title: "Sous Chef", full_time: true)
+    @susie = @nice_smellin_sals.chefs.create!(name: "Susie Mae", age: 21, title: "Assistant Sous Chef", full_time: true)
+    @bellatrix = @nice_smellin_sals.chefs.create!(name: "Bellatrix Le Yum", age: 38, title: "Pastry Chef", full_time: true)
+    @crusty = @stinky_petes.chefs.create!(name: "Crusty Calvin", age: 104, title: "Sous Chef", full_time: false)
+    @slimy = @stinky_petes.chefs.create!(name: "Slimy Susan", age: 78, title: "Assistant Sous Chef", full_time: false)
+    @nasty = @stinky_petes.chefs.create!(name: "Nasty Norman", age: 12, title: "Owner", full_time: true)  
+  end
+
+  it 'displays the names of all the chefs' do
+    visit '/chefs'
+    # save_and_open_page
+    expect(page).to have_content(@stephanie.name)
+    expect(page).to have_content(@susie.name)
+    expect(page).to have_content(@bellatrix.name)
+    expect(page).to have_content(@crusty.name)
+    expect(page).to have_content(@slimy.name)
+    expect(page).to have_content(@nasty.name)
+  end
+end
