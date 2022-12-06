b21ac80be1ae07d12f0a77d0b2733856d9a1d332
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-12-05 17:01:52 -0800

    allow the user to add new artisits

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,18 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    restaurant = Restaurant.create!(name: params[:name], city: params[:city], star_rating: params[:star_rating], osha_safety_certified: [:osha_safety_certified])
+    redirect_to "/restaurants/#{restaurant.id}"
+  end
+
+  private
+    def restaurant_params
+      
+    end
 end
